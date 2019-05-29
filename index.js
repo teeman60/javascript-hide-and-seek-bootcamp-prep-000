64544d02c0223b2c243d51be4f8eb342f875919f
Taiye Salami <taiye41@gmail.com>
2019-05-29 12:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node')
   var deepestNode = grandNode.children[0]
-  for (var i = 0; deepestNode.children[i]; i++) {
+  for (var i = 0; deepestNode.children[i]; i) {
     deepestNode = deepestNode.children[0]
   }
   return deepestNode
